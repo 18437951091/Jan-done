be6d3b04f6cfef9cb60f1bfc36f2734958def7c1
yangmn <yang_mn@126.com>
2022-02-09 10:39:52 +0800

    add target

---
+++ src/components/Target.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>
+    新的一年，定一下2022年的目标：
+  </h3>
+  <hr>
+
+  <p>
+    <input id="target1" type="checkbox" value="first_checkbox">
+    <label for="target1" class="ml-2">拿到驾照</label>
+  </p>
+  <p>
+    <input id="target2" type="checkbox" value="first_checkbox">
+    <label for="target2" class="ml-2">CCAA考试通过</label>
+  </p>
+  <p>
+    <input id="target3" type="checkbox" value="first_checkbox">
+    <label for="target3" class="ml-2">审核员注册成功</label>
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
